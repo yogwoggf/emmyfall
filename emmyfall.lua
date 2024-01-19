f94e99e18829b7791d2b66486029cb5516ec5466
yogwoggf <yogwoggf@users.noreply.github.com>
2024-01-19 00:54:43 +0000

    deploy: ca43228dd8d311ec55756e1d9b6f08106f31030c

--- emmyfall.lua
+++ emmyfall.lua
@@ -120,22 +120,22 @@
 --- coroutine
 ---  Coroutine library
 _G.coroutine = {}
-	--- status - shared - libs_sh/coroutine.lua#L108
+	--- status - shared - libs_sh/coroutine.lua#L109
 	---@param coroutine thread The coroutine
 	---@return string undefined Either "suspended", "running", "normal" or "dead"
 	function _G.coroutine.status(coroutine) end
-	--- yield - shared - libs_sh/coroutine.lua#L96
+	--- yield - shared - libs_sh/coroutine.lua#L97
 	---@param Optional ... parameters that will be returned to the main thread
 	---@return ... undefined Any values passed to the coroutine
 	function _G.coroutine.yield(Optional) end
-	--- running - shared - libs_sh/coroutine.lua#L116
+	--- running - shared - libs_sh/coroutine.lua#L118
 	---@return thread undefined Currently running coroutine
 	function _G.coroutine.running() end
 	--- wrap - shared - libs_sh/coroutine.lua#L78
 	---@param func function Function of the coroutine
 	---@return function undefined A function that, when called, resumes the created coroutine. Any parameters to that function will be passed to the coroutine.
 	function _G.coroutine.wrap(func) end
-	--- wait - shared - libs_sh/coroutine.lua#L123
+	--- wait - shared - libs_sh/coroutine.lua#L125
 	---@param time number Time in seconds to suspend the coroutine
 	function _G.coroutine.wait(time) end
 	--- create - shared - libs_sh/coroutine.lua#L69

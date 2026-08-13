be10d64fc662b8e768af647ce32e6b70f3d5291e
yogwoggf <yogwoggf@users.noreply.github.com>
2026-08-13 07:20:35 +0000

    deploy: ca43228dd8d311ec55756e1d9b6f08106f31030c

--- emmyfall.lua
+++ emmyfall.lua
@@ -3898,7 +3898,7 @@ _G["OWNER"] = nil
 ---  Constant that denotes whether the code is executed on the client
 ---@type any
 _G["CLIENT"] = nil
-	--- localToWorld - shared - libs_sh/builtins.lua#L1228
+	--- localToWorld - shared - libs_sh/builtins.lua#L1224
 	---@param localPos Vector The position vector that should be translated to world coordinates
 	---@param localAng Angle The angle that should be converted to a world angle
 	---@param originPos Vector The origin point of the source coordinate system, in world coordinates
@@ -3929,7 +3929,7 @@ _G["CLIENT"] = nil
 	---@param base number? The numerical base of the digits in the string. Must be between 2 and 36. Default is 10.
 	---@return number? undefined The number representation of the input value or nil if it couldn't be converted.
 	function _G.tonumber(value, base) end
-	--- throw - shared - libs_sh/builtins.lua#L1172
+	--- throw - shared - libs_sh/builtins.lua#L1168
 	---@param msg string Message string
 	---@param level number? Which level in the stacktrace to blame. Defaults to 1
 	---@param uncatchable boolean? Makes this exception uncatchable
@@ -3965,7 +3965,7 @@ _G["CLIENT"] = nil
 	--- print - shared - libs_sh/builtins.lua#L585
 	---@param printArgs ... Values to print. Colors before text will set the text color
 	function _G.print(printArgs) end
-	--- pcall - shared - libs_sh/builtins.lua#L1094
+	--- pcall - shared - libs_sh/builtins.lua#L1090
 	---@param func function Function to be executed and of which the errors should be caught of
 	---@param arguments ... Arguments to call the function with.
 	---@return boolean undefined If the function had no errors occur within it.
@@ -3974,7 +3974,7 @@ _G["CLIENT"] = nil
 	--- setSoftQuota - shared - libs_sh/builtins.lua#L333
 	---@param quota number The threshold where the soft error will be thrown. Ratio of current cpu to the max cpu usage. 0.5 is 50%
 	function _G.setSoftQuota(quota) end
-	--- isValid - shared - libs_sh/builtins.lua#L1197
+	--- isValid - shared - libs_sh/builtins.lua#L1193
 	---@param object any Table to check
 	---@return boolean undefined If it is valid
 	function _G.isValid(object) end
@@ -4055,14 +4055,14 @@ _G["CLIENT"] = nil
 	--- ramAverage - shared - libs_sh/builtins.lua#L284
 	---@return number undefined The ram used in kilobytes
 	function _G.ramAverage() end
-	--- xpcall - shared - libs_sh/builtins.lua#L1123
+	--- xpcall - shared - libs_sh/builtins.lua#L1119
 	---@param func function The function to call initially.
 	---@param callback function The function to be called if execution of the first fails; the error message and stack trace are passed.
 	---@param passArgs ... Varargs to pass to the initial function.
 	---@return boolean undefined Status of the execution; true for success, false for failure.
 	---@return ... undefined The returns of the first function if execution succeeded, otherwise the return values of the error callback.
 	function _G.xpcall(func, callback, passArgs) end
-	--- class - shared - libs_sh/builtins.lua#L1291
+	--- class - shared - libs_sh/builtins.lua#L1287
 	---@param name string The string name of the class
 	---@param super table? The (optional) parent class to inherit from
 	function _G.class(name, super) end
@@ -4070,14 +4070,14 @@ _G["CLIENT"] = nil
 	---@param x any Input to check
 	---@return boolean undefined If the object is a boolean or not
 	function _G.isbool(x) end
-	--- restart - shared - libs_sh/builtins.lua#L1267
+	--- restart - shared - libs_sh/builtins.lua#L1263
 	---@param chip Entity? The chip to restart. If nil, it will restart the current chip.
 	function _G.restart(chip) end
-	--- enableHud - shared - libs_sh/builtins.lua#L1247
+	--- enableHud - shared - libs_sh/builtins.lua#L1243
 	---@param ply Player The player to enable the hud on. If CLIENT, will be forced to player()
 	---@param active boolean Whether hud hooks should be active. true to force on, false to force off.
 	function _G.enableHud(ply, active) end
-	--- worldToLocal - shared - libs_sh/builtins.lua#L1209
+	--- worldToLocal - shared - libs_sh/builtins.lua#L1205
 	---@param pos Vector The position that should be translated from the current to the new system
 	---@param ang Angle The angles that should be translated from the current to the new system
 	---@param newSystemOrigin Vector The origin of the system to translate to
@@ -4085,19 +4085,19 @@ _G["CLIENT"] = nil
 	---@return Vector undefined localPos
 	---@return Angle undefined localAngles
 	function _G.worldToLocal(pos, ang, newSystemOrigin, newSystemAngles) end
-	--- assert - shared - libs_sh/builtins.lua#L1189
+	--- assert - shared - libs_sh/builtins.lua#L1185
 	---@param expression any Anything that will be evaluated to be true or false
 	---@param msg string? Error message. Default "assertion failed!"
 	---@param args ... Any arguments to return if the assertion is successful
 	function _G.assert(expression, msg, args) end
-	--- error - shared - libs_sh/builtins.lua#L1180
+	--- error - shared - libs_sh/builtins.lua#L1176
 	---@param msg string Message string
 	---@param level number? Which level in the stacktrace to blame. Defaults to 1. 0 for no stacktrace.
 	function _G.error(msg, level) end
 	--- chip - shared - libs_sh/builtins.lua#L57
 	---@return Entity undefined Starfall chip entity
 	function _G.chip() end
-	--- try - shared - libs_sh/builtins.lua#L1151
+	--- try - shared - libs_sh/builtins.lua#L1147
 	---@param func function Function to execute
 	---@param catch function? Optional function to execute in case func fails
 	function _G.try(func, catch) end
@@ -4106,7 +4106,7 @@ _G["CLIENT"] = nil
 	---@param loadpriority table Table of files that should be loaded before any others in the directory
 	---@return table undefined Table of return values of the scripts
 	function _G.dodir(path, loadpriority) end
-	--- getMethods - shared - libs_sh/builtins.lua#L1014
+	--- getMethods - shared - libs_sh/builtins.lua#L1010
 	---@param sfType string Name of SF type
 	---@return table undefined Table of the type's methods which can be edited or iterated
 	function _G.getMethods(sfType) end
@@ -4267,18 +4267,18 @@ _G["CLIENT"] = nil
 --- debug
 ---  Lua debug library https://wiki.garrysmod.com/page/Category:debug
 _G.debug = {}
-	--- traceback - shared - libs_sh/builtins.lua#L1028
+	--- traceback - shared - libs_sh/builtins.lua#L1024
 	---@param A thread? thread to get the stack trace of. If nil, this argument will be used as the message and the current thread becomes the target.
 	---@param message string? A message to be included at the beginning of the stack trace. Default: ""
 	---@param stacklevel number? Which position in the execution stack to start the traceback at. Default: 1
 	---@return string undefined A dump of the execution stack.
 	function _G.debug.traceback(A, message, stacklevel) end
-	--- getlocal - shared - libs_sh/builtins.lua#L1071
+	--- getlocal - shared - libs_sh/builtins.lua#L1067
 	---@param funcOrStackLevel function|number Function or stack level to get info about. Defaults to stack level 0.
 	---@param index number The index of the local to get
 	---@return string undefined The name of the local
 	function _G.debug.getlocal(funcOrStackLevel, index) end
-	--- getinfo - shared - libs_sh/builtins.lua#L1055
+	--- getinfo - shared - libs_sh/builtins.lua#L1051
 	---@param funcOrStackLevel function|number Function or stack level to get info about. Defaults to stack level 0.
 	---@param fields string? A string that specifies the information to be retrieved. Defaults to all (flnSu).
 	---@return table undefined DebugInfo table
